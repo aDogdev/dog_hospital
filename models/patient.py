d1e96153750e3945f1ccb10eea70060ab32b50c0
aDog <adrianmg3008@gmail.com>
2024-04-03 14:49:20 -0400

    add patient model

---
+++ models/patient.py
@@ -0,0 +1,10 @@
+from odoo import fields, models, api
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age")
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
