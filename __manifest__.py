695821cc13f164756b21e800d7393ba0eaa1b41c
aDog <adrianmg3008@gmail.com>
2024-04-03 13:08:28 -0400

    create addon, init  manifest files

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Dog Hospital Management",
+    "version": "1.0.0",
+    "description": "Hospital Management system",
+    "summary": "Hospital Management system",
+    "author": "aDog",
+    "website": "",
+    "license": "LGPL-3",
+    "category": "Hospital",
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "demo": [],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
