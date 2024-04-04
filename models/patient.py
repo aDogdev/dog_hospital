7c772e67002dd8d820dcbe75a09d17aaad76cbf1
aDog <adrianmg3008@gmail.com>
2024-04-04 17:41:07 -0400

    add tracking fields to chatter

--- models/patient.py
+++ models/patient.py
@@ -6,8 +6,10 @@ class HospitalPatient(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Hospital Patient"
 
-    name = fields.Char(string="Name", required=True)
-    ref = fields.Char(string="Reference")
-    age = fields.Integer(string="Age")
-    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
+    name = fields.Char(string="Name", required=True, tracking=True)
+    ref = fields.Char(string="Reference", tracking=True)
+    age = fields.Integer(string="Age", tracking=True)
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female")], string="Gender", tracking=True
+    )
     active = fields.Boolean(default=True)
