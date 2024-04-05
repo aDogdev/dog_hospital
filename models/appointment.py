a095b24afef783fe9ec7853bab4af36925976809
aDog <adrianmg3008@gmail.com>
2024-04-05 11:16:05 -0400

    add tracking to appointment model

--- models/appointment.py
+++ models/appointment.py
@@ -8,7 +8,7 @@ class HospitalAppointment(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Hospital Appointment"
 
-    patient_id = fields.Many2one("hospital.patient", string="Patient")
+    patient_id = fields.Many2one("hospital.patient", string="Patient", tracking=True)
     gender = fields.Selection(
         string="Gender",
         related="patient_id.gender",
@@ -18,5 +18,8 @@ class HospitalAppointment(models.Model):
         default=lambda self: datetime.combine(
             fields.Datetime.today() + relativedelta(months=1), time(13, 0)
         ),
+        tracking=True,
     )
-    booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
+    booking_date = fields.Date(
+        string="Booking Date", default=fields.Date.context_today, tracking=True
+    )
