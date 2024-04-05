acbc2142f1338a517c7a980058702b2b41ebd5f2
aDog <adrianmg3008@gmail.com>
2024-04-04 21:04:24 -0400

    add patient_id field

--- models/appointment.py
+++ models/appointment.py
@@ -4,3 +4,5 @@ from odoo import fields, models, api
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _description = "Hospital Appointment"
+
+    patient_id = fields.Many2one("hospital.patient", string="Patient")
