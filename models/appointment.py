dd50b218747e3919452e740cb53f04b649dd66b9
aDog <adrianmg3008@gmail.com>
2024-04-05 10:11:29 -0400

    add chatter to appointment form view

--- models/appointment.py
+++ models/appointment.py
@@ -5,6 +5,7 @@ from dateutil.relativedelta import relativedelta
 
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Hospital Appointment"
 
     patient_id = fields.Many2one("hospital.patient", string="Patient")
