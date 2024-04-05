2d8bbe1125bfa6dbf13ef3c813362ddda59777df
aDog <adrianmg3008@gmail.com>
2024-04-05 00:10:44 -0400

    add appointment_time and booking_date fields

--- models/appointment.py
+++ models/appointment.py
@@ -6,3 +6,5 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
 
     patient_id = fields.Many2one("hospital.patient", string="Patient")
+    appointment_time = fields.Datetime(string="Appointment Time")
+    booking_date = fields.Date(string="Booking Date")
